d0c526f6b0a646d7bc1929de320745801958a917
Square-Face <sq8.square.face@gmail.com>
2023-12-14 00:29:02 +0100

    started work on parser

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,38 @@
-pub fn validate(input: &String)->bool {
-    return false;
-}
+pub fn parse(input: &String) -> Result<[u8;10], &'static str> {
+    //! Parses a given input string into a array of the individual integers
+    //!
+    //! # Arguments
+    //! * `input` The input string to parse
+    //! 
+    //! # Returns
+    //! * `Result<[u8;10], &'static str>` Array of numbers or a message explaining why pin is
+    //! invalid
 
-pub fn parse(input: &String)->[u8;10] {
-    return [0;10];
+    if !input.is_ascii() {
+        return Err("Not valid ascii")
+    }
+
+    match input.len() {
+        10 => {}
+        11 => {
+            let chars = input.as_bytes();
+            
+            if chars[7] != 0x2D || chars[7] != 0x2B { // check for - or + in the 7th spot
+                return Err("7th char must be - or +")
+            }
+        }
+        _ => {
+            // Length is invalid
+            
+            if input.len() > 11 {
+                return Err("Too long")
+            }
+
+            if input.len() < 10 {
+                return Err("Too short")
+            }
+        }
+    }
+
+    return Ok([0;10]);
 }
